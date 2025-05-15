efb2f57643e617892c6c33b3806f6ad436176deb
astha-innov <astha.04122005@gmail.com>
2025-05-15 08:44:24 +0530

    Age_finder.py

---
+++ Age_finder.py
@@ -0,0 +1,8 @@
+# To find out your age
+name = input('Heya! What is your name? ')
+birth_year = int(input('What is your birth year? '))
+print('Well, Thank you '+name)
+now_year = int(input('and may i know which year is this ?'))
+age = now_year-birth_year
+print('Your age is ',age)
+print('Thank You for using our age checker.')
