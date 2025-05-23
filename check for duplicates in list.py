069cbe9f0126e779a5cfc2cf2c4b416391f93ecf
astha-innov <astha.04122005@gmail.com>
2025-05-23 19:26:42 +0530

    Update check for duplicates in list.py

--- check for duplicates in list.py
+++ check for duplicates in list.py
@@ -6,4 +6,14 @@ for i in range(len(some_list)):
         if some_list[i]==some_list[j] and some_list[i] not in duplicates:
             duplicates.append(some_list[i])
 print(duplicates)
+
+
+# check for duplicates in list
+some_list = ['a','b','c','d','e','d','c','a','b','a']
+duplicates = []
+for i in some_list:
+    if some_list.count(i) > 1 and i not in duplicates:
+        duplicates.append(i)
+print(duplicates)
+        
     
